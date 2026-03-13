30f3f06fdedb2838f17c44269724885f80d3c209
Juangalvez1 <juanlopezjosegalvez@gmail.com>
2026-03-13 18:30:06 -0500

    Checking division by 0 in task 4

--- Parcial1/Punto4.py
+++ Parcial1/Punto4.py
@@ -6,11 +6,13 @@ def main():
     secondDigit = (num % 1000) // 100
     thirdDigit = (num % 100) // 10
     fourthDigit = num % 10
-
-    if firstDigit % fourthDigit == 0:
-        print(f"{firstDigit} SI es multiplo de {fourthDigit}")
+    if fourthDigit != 0:
+        if firstDigit % fourthDigit == 0:
+            print(f"\n{firstDigit} SI es multiplo de {fourthDigit}")
+        else:
+            print(f"\n{firstDigit} NO es multiplo de {fourthDigit}")
     else:
-        print(f"{firstDigit} NO es multiplo de {fourthDigit}")
+        print(f"El cuarto digito es '{fourthDigit}' entonces no se puede comprobar si el primer digito es multiplo del cuarto digito.\n")
 
     sumSecondThird = secondDigit + thirdDigit
     print (f"La suma del secondDigit y el tercer numero es: {sumSecondThird}")
